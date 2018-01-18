a1824e3a96c879363ba197b77b10dfb7a83f94d5
Rene <renemf77@gmail.com>
2018-01-17 21:37:20 -0800

    Add Exception

---
+++ Exceptions/InvalidLoginException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsrSrvcDAL.Exceptions {
+    public abstract class InvalidLoginException : System.Exception {
+
+        public InvalidLoginException()
+            : base() {
+        }
+
+
+        public InvalidLoginException(String message)
+            : base(message) {
+        }
+
+
+        public InvalidLoginException(String message, Exception innerException)
+            : base(message, innerException) {
+        }
+
+
+
+    }
+}
